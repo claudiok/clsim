09d04d7a8e837c3f870e422b4e34ce2e64296bf2
Claudio Kopper <ckopper@icecube.wisc.edu>
2012-03-30 15:26:36 +0000

    added some cluster submission script examples
    git-svn-id: http://code.icecube.wisc.edu/svn/sandbox/ckopper/clsim/trunk@86710 16731396-06f5-0310-8873-f7f720988828

---
+++ resources/scripts/cluster_submission/npx3/applyCLSim.GPU.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python
+
+from optparse import OptionParser
+import os
+import string
+
+usage = "usage: %prog [options] inputfile"
+parser = OptionParser(usage)
+parser.add_option("-o", "--outfile", default="test_muons_photons.i3",
+                  dest="OUTFILE", help="Write output to OUTFILE (.i3{.gz} format)")
+parser.add_option("-i", "--infile", default="test_muons.i3",
+                  dest="INFILE", help="Read input from INFILE (.i3{.gz} format)")
+parser.add_option("-s", "--seed",type="int",default=12345,
+                                  dest="SEED", help="Initial seed for the random number generator")
+parser.add_option("-r", "--runnumber", type="int", default=1,
+                  dest="RUNNUMBER", help="The run number for this simulation")
+parser.add_option("-p", "--max-parallel-events", type="int", default=100,
+                  dest="MAXPARALLELEVENTS", help="maximum number of events(==frames) that will be processed in parallel")
+parser.add_option("--apply-mmc", action="store_true", default=False,
+                  dest="APPLYMMC", help="apply MMC to the I3MCTree before passing it to CLSim")
+parser.add_option("--remove-photon-data", action="store_true", default=False,
+                  dest="REMOVEPHOTONDATA", help="Remove I3Photons before writing the output file (only keep hits)")
+
+# parse cmd line args, bail out if anything is not understood
+(options,args) = parser.parse_args()
+if len(args) != 0:
+        crap = "Got undefined options:"
+        for a in args:
+                crap += a
+                crap += " "
+        parser.error(crap)
+
+########################
+if options.INFILE:
+        filename = options.INFILE
+        if os.access(filename,os.R_OK) == False:
+                raise "cannot find input file!"
+        infile = filename
+        print 'using input file %s' % infile
+else:
+        print "No input file!"
+        parser.print_help()
+        exit(-1)
+
+infileRoot, infileExt = os.path.splitext(infile)
+if infileExt == ".gz":
+    infileRoot2, infileExt2 = os.path.splitext(infileRoot)
+    if infileExt2 == ".i3":
+        infileRoot=infileRoot2
+        infileExt = ".i3.gz"
+
+if infileExt != ".i3" and infileExt != ".i3.gz":
+        raise Exception, "you have to specify either a .i3 or an .i3.gz file!"
+
+########################
+outdir=""
+outfile=None
+if options.OUTFILE:
+        outfile = options.OUTFILE
+        # did the user specify a directory? then use that and auto-generate
+        if os.path.isdir(outfile):
+            outdir = outfile
+            outfile = None
+        else:
+            outdir, outfile = os.path.split(outfile)
+
+# add a trailing slash to the output directory name if not already there
+if outdir and outdir!="":
+    if outdir[-1] != "/":
+        outdir += "/"
+
+if not outfile:
+        # automatically generate the output filename
+        infileRootDir, infileRootFile = os.path.split(infileRoot)
+        outfile = infileRootFile + "_clsim"
+        outfile = outfile + infileExt
+print "output dir is %s" % outdir
+print "output file is %s" % outdir + outfile
+
+########################
+
+if options.REMOVEPHOTONDATA:
+    print "not storing I3Photons"
+else:
+    print "storing I3Photons"
+
+import os
+
+# try to adapt to condor stupidity
+if "_CONDOR_SLOT" in os.environ: # running in condor?
+    if "CUDA_VISIBLE_DEVICES" in os.environ:
+        print "running in CONDOR, but CUDA_VISIBLE_DEVICES is already set. no further configuration necessary."
+    else:
+        condorSlotNumber = int(os.environ["_CONDOR_SLOT"])
+        print "script seems to be running in condor (slot %u). auto-configuring CUDA_VISIBLE_DEVICES!" % condorSlotNumber
+        os.environ["CUDA_VISIBLE_DEVICES"] = str(condorSlotNumber-1)
+
+
+
+from I3Tray import *
+from os.path import expandvars
+import sys
+
+from icecube import icetray, dataclasses, dataio, phys_services
+from icecube import clsim
+
+# a random number generator
+randomService = phys_services.I3SPRNGRandomService(
+    seed = options.SEED,
+    nstreams = 10000,
+    streamnum = options.RUNNUMBER)
+
+if options.APPLYMMC:
+    load("libc2j-icetray")
+    load("libmmc-icetray")
+    MMCseed=options.SEED
+
+tray = I3Tray()
+
+
+tray.AddModule("I3Reader","reader",
+               Filename=infile)
+
+if options.APPLYMMC:
+    mmcOpts = "-seed=%i -radius=900 -length=1600" % (MMCseed)
+    
+    tray.AddModule("I3PropagatorMMC","propagate",
+                   PrimaryTreeName = "I3MCTree",
+                   mode=-1,
+                   opts=mmcOpts,
+                   ShiftParticles = False,
+                   )
+
+
+
+if options.REMOVEPHOTONDATA:
+    photonSeriesName = None
+else:
+    photonSeriesName = "PropagatedPhotons"
+
+tray.AddSegment(clsim.I3CLSimMakeHits, "makeCLSimHits",
+    PhotonSeriesName = photonSeriesName,
+    ParallelEvents = options.MAXPARALLELEVENTS,
+    RandomService = randomService,
+    UseGPUs=True,
+    UseCPUs=False,
+    IceModelLocation=expandvars("$I3_SRC/clsim/resources/ice/spice_mie"))
+
+tray.AddModule("I3Writer","writer",
+    Filename = outdir+outfile)
+
+tray.AddModule("TrashCan", "the can")
+
+tray.Execute()
+tray.Finish()
