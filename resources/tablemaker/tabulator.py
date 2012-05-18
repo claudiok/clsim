1b080fb6d0e9e0f71fae08e00b6bd48d64ad1c07
Jakob van Santen <jvansanten@gmail.com>
2012-05-18 17:13:48 +0000

    Tabulator, now with both a module and a way to store histograms in FITS files. Nice, but also completely untested.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/clsim/trunk@88435 16731396-06f5-0310-8873-f7f720988828

--- resources/tablemaker/tabulator.py
+++ resources/tablemaker/tabulator.py
@@ -1,8 +1,8 @@
 
-from icecube.icetray import I3Units
+from icecube.icetray import I3Units, I3Module
 from icecube.dataclasses import I3Position, I3Particle, I3Direction, I3Constants
 from icecube.phys_services import I3Calculator
-from icecube.clsim import I3Photon
+from icecube.clsim import I3Photon, GetIceCubeDOMAcceptance, GetIceCubeDOMAngularSensitivity
 import numpy, numpy_extensions, math
 
 def potemkin_photon():
@@ -12,7 +12,10 @@ def potemkin_photon():
 	ph.startPos = I3Position(0,0,0)
 	ph.startTime = 0.
 	ph.pos = I3Position(100., 100., 100.,)
-	ph.AppendToIntermediatePositionList(I3Position(100., 0., 0.,))
+	ph.AppendToIntermediatePositionList(I3Position(100., 0., 0.,), 1)
+	ph.distanceInAbsorptionLengths = 2
+	ph.wavelength = 450.
+	ph.weight = 1.
 	
 	return ph
 
@@ -26,9 +29,69 @@ def potemkin_source():
 	
 	return p
 	
+	
+class PhotoTable(object):
+	
+	def __init__(self, binedges, values, weights, n_photons=0):
+		self.binedges = binedges
+		self.values = values
+		self.weights = weights
+		self.bincenters = [(edges[:1]+edges[:-1])/2. for edges in self.binedges]
+		self.n_photons = n_photons
+		
+	def __iadd__(self, other):
+		if not isinstance(other, self.__class__):
+			raise TypeError, "Can only add another %s" % (self.__class__.__name__)
+		self.values += other.values
+		self.weights += other.weights
+		self.n_photons += other.n_photons
+		
+	def __imul__(self, num):
+		self.values *= num
+		self.weights *= num*num
+		
+	def normalize(self):
+		self *= (1./self.n_photons)
+		
+	def save(self, fname):
+		import pyfits
+		data = pyfits.PrimaryHDU(self.values)
+		data.header.update('TYPE', 'Photon detection probability table')
+		
+		data.header.update('N_PHOTONS', n_photons)
+		
+		hdulist = pyfits.HDUList([data])
+		
+		if self.weights is not None:
+			errors = pyfits.ImageHDU(self.weights, name='ERRORS')
+			hdulist.append(errors)
+		
+		for i in xrange(self.values.ndim):
+			edgehdu = pyfits.ImageHDU(self.binedges[i],name='EDGES%d' % i)
+			hdulist.append(edgehdu)
+			
+		hdulist.writeto(fname)
+		
+	@classmethod
+	def load(cls, fname):
+		hdulist = pyfits.open(fname)
+		data = hdulist[0]
+		values = data.data
+		binedges = []
+		for i in xrange(values.ndim):
+			binedges.append(hdulist['EDGES%d' % i])
+		try:
+			weights = hdulist['ERRORS']
+		except KeyError:
+			weights = None
+			
+		n_photons = data.header['N_PHOTONS']
+			
+		return cls(binedges, values, n_photons, weights)
+	
 class Tabulator(object):
 	
-	def __init__(self, source, nbins=(10, 10, 10, 10)):
+	def __init__(self, nbins=(10, 10, 10, 10)):
 		self.binedges = [
 		    numpy.linspace(0, numpy.sqrt(600), nbins[0]+1)**2,
 		    numpy.linspace(-1, 1, nbins[1]+1),
@@ -38,11 +101,16 @@ class Tabulator(object):
 		self.values = numpy.zeros(tuple([len(v)-1 for v in self.binedges]), dtype=numpy.double)
 		self.weights = numpy.zeros(self.values.shape)
 		self.n_photons = 0
-		self.source = source
 		self._step_length = 1.*I3Units.m
-		self._c = I3Constants.c/I3Constants.n_ice_group # FIXME: actually depends on local medium and wavelength!
+		self._angularEfficiency = GetIceCubeDOMAngularSensitivity()
+		self._effectiveArea = GetIceCubeDOMAcceptance()
 		
-	def _normalize(self):
+	def save(self, fname):
+		# normalize to a photon flux, but not the number of photons (for later stacking)
+		area = self._getBinAreas()
+		PhotoTable(self.binedges, self.values/area, self.weights/(area*area)).save(fname)
+	
+	def _getBinAreas(self):
 		
 		near = numpy.meshgrid_nd(*[b[:-1] for b in self.binedges[:-1]])
 		far = numpy.meshgrid_nd(*[b[1:] for b in self.binedges[:-1]])
@@ -50,7 +118,11 @@ class Tabulator(object):
 		# The effective area of the bin is its volume divided by
 		# the sampling frequency
 		area = ((far[0]**3-near[0]**3)/3.)*(far[1]-near[1])*(far[2]-near[2])/self._step_length
-		fluxconst = area*self.n_photons
+		return area
+	
+	def _normalize(self):
+		
+		area = self._getBinAreas()
 		
 		# convert each weight (in units of photons * m^2) to a probability
 		self.values /= fluxconst
@@ -67,17 +139,22 @@ class Tabulator(object):
 		else:
 			return numpy.array([1., 0., 0.])
 
-	def _getBinIndices(self, pos, dt):
+	def _getBinIndices(self, source, pos, t):
 		"""
-		Sperical case only!
+		This is the only function that needs to be significantly modified
+		to support different table geometries.
+		
+		NB: the currently implements sperical, source-centered coordinates only!
 		
 		TODO: implement detector-centered (degenerate) cylindrical coords
 		"""
-		p0 = numpy.array(self.source.pos)
+		p0 = numpy.array(source.pos)
 		p1 = numpy.array(pos)
 		
-		source_dir = numpy.array([self.source.dir.x, self.source.dir.y, self.source.dir.z])
-		perp_dir = self._getPerpendicularDirection(self.source.dir)
+		dt = I3Calculator.time_residual(source, pos, t, I3Constants.n_ice_group, I3Constants.n_ice_phase)
+		
+		source_dir = numpy.array([source.dir.x, source.dir.y, source.dir.z])
+		perp_dir = self._getPerpendicularDirection(source.dir)
 		
 		norm = lambda arr: numpy.sqrt((arr**2).sum())
 		
@@ -96,14 +173,29 @@ class Tabulator(object):
 				idx[j] = len(self.binedges[j])-2
 		return tuple(idx)
 
-	def _getTimeResidual(self, pos, t):
-		return I3Calculator.time_residual(self.source, pos, t, I3Constants.n_ice_group, I3Constants.n_ice_phase)
-
-	def recordPhoton(self, photon):
+	def recordPhoton(self, source, photon, n_photons=1):
+		"""
+		:param nphotons: number of photons to add to the total tally
+		after recording. Set this to zero if you're going to do your own counting.
+		"""
+		# position of photon at each scatter
 		positions = photon.positionList
+		# path length in units of absorption length at each scatter
+		abs_lengths = [photon.GetDistanceInAbsorptionLengthsAtPositionListEntry(i) for i in xrange(len(positions))]
+		
+		# The various weights are constant for different bits of the photon track.
+		# Constant for a given photon:
+		# - Wavelength bias: 1 / photon weight
+		# - DOM effective area (as a function of wavelength)
+		# Constant for paths between scatters:
+		# - relative angular efficiency (as a function of impact angle)
+		# Varies along the track:
+		# - survival probability (exp(-path/absorption length))
+		# The units of the combined weight are m^2 / photon
+		wlen_weight = self._effectiveArea.GetValue(photon.wavelength)/photon.weight
+		
 		t = photon.startTime
-		# TODO: get the actual absorption lengths for each intermediate step
-		abs_lengths = numpy.arange(len(positions))
+		
 		for start, start_lengths, stop, stop_lengths in zip(positions[:-1], abs_lengths[:-1], positions[1:], abs_lengths[1:]):
 			pdir = self._getDirection(start, stop)
 			# XXX HACK: the cosine of the impact angle with the
@@ -114,22 +206,58 @@ class Tabulator(object):
 			# segment length in units of the local absorption length
 			abs_distance = stop_lengths-start_lengths 
 			
+			impact_weight = wlen_weight*self._angularEfficiency.GetValue(impact)
+			
 			# FIXME: this samples at fixed distances. Randomize instead?
 			for d in numpy.arange(0, distance, self._step_length):
 				# Which bin did we land in?
 				pos = I3Position(*(numpy.array(start) + d*pdir))
-				dt = self._getTimeResidual(pos, t + d/photon.groupVelocity)
-				indices = self._getBinIndices(pos, dt)
-			
-				# TODO: add extra weights:
-				#       - wavelength
-				#       - DOM area
-				#       - DOM angular sensitivity
-				# weight will be multiplied by the DOM effective area, so units will be photons * m^2
+				indices = self._getBinIndices(source, pos, t + d/photon.groupVelocity)
+				
 				d_abs = start_lengths + (d/distance)*abs_distance
-				weight = numpy.exp(-d_abs)
+				weight = impact_weight*numpy.exp(-d_abs)
+				
 				self.values[indices] += weight
 				self.weights[indices] += weight*weight
 			
 			t += distance/photon.groupVelocity
-		self.n_photons += 1
+		self.n_photons += n_photons
+		
+class I3TabulatorModule(I3Module, Tabulator):
+	def __init__(self, ctx):
+		I3Module.__init__(self, ctx)
+		Tabulator.__init__(self)
+		
+		self.AddParameter("Filename", "Output filename", "foo.fits")
+		self.AddParameter("Photons", "Name of I3PhotonSeriesMap in the frame", "I3PhotonSeriesMap")
+		self.AddParameter("Statistics", "Name of the CLSimStatistics object in the frame", "")
+		self.AddParameter("Source", "Name of the source I3Particle in the frame", "")
+		self.AddOutBox("OutBox")
+		
+	def Configure(self):
+		
+		self.fname = self.GetParameter("Filename")
+		self.photons = self.GetParameter("Photons")
+		self.stats = self.GetParameter("Statistics")
+		self.source = self.GetParameter("Source")
+		
+	def DAQ(self, frame):
+		source = frame[self.source]
+		photonmap = frame[self.photons]
+		
+		for photons in photonmap.itervalues():
+			for photon in photons:
+				self.recordPhoton(source, photon, 0)
+		
+		# Each I3Photon can only carry a fixed number of intermediate
+		# steps, so there may be more than one I3Photon for each generated
+		# photon.
+		stats = frame[self.stats]
+		self.n_photons += stats.GetTotalNumberOfPhotonsGenerated()
+		
+		self.PushFrame(frame)
+		
+	def Finish(self):
+		
+		self.save(self.fname)
+	 
