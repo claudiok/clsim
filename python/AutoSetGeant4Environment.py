77309f3c51c5116f92e590a7425779482e30a44d
Claudio Kopper <ckopper@icecube.wisc.edu>
2012-09-18 16:20:46 +0000

    oops
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/clsim/trunk@93086 16731396-06f5-0310-8873-f7f720988828

--- python/AutoSetGeant4Environment.py
+++ python/AutoSetGeant4Environment.py
@@ -88,7 +88,7 @@ def AutoSetGeant4Environment(force=True):
                 raise RuntimeError("Cannot automatically set missing environment variables. ($I3_PORTS/bin/geant4.sh is missing.) Please set them yourself.")
 
             # get the environment after loading geant4.sh
-            source = os.path.expandvars(". $I3_PORTS/bin/gea2nt4.sh")
+            source = os.path.expandvars(". $I3_PORTS/bin/geant4.sh")
             dump = '/usr/bin/env python -c "import os,pickle;print pickle.dumps(os.environ)"'
             penv = subprocess.Popen('%s && %s' %(source,dump), stdout=subprocess.PIPE, shell=True).communicate()[0]
             #penv = subprocess.check_output('%s && %s' %(source,dump), shell=True)
