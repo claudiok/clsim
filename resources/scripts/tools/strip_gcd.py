44b0c4f24ba42133600a30b50438e5ac7789608d
Claudio Kopper <ckopper@icecube.wisc.edu>
2012-11-21 17:14:16 +0000

    modernize this script
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/clsim/trunk@95809 16731396-06f5-0310-8873-f7f720988828

--- resources/scripts/tools/strip_gcd.py
+++ resources/scripts/tools/strip_gcd.py
@@ -18,23 +18,18 @@ output_file = sys.argv[2]
 import os
 import sys
 
-load("libdataclasses")
-load("libdataio")
+from icecube import icetray, dataclasses, dataio
 
 tray = I3Tray()
 
-tray.AddModule("I3Reader","reader")(
-    ("FileNameList", [input_file])
-    )
+tray.AddModule("I3Reader","reader",
+    FileNameList = [input_file])
 
-#tray.AddModule("Dump","dump")
-
-tray.AddModule("I3Writer", "writer")(
-    ("Filename", output_file),
-    ("Streams", ["Physics"]),
-    )
+tray.AddModule("I3Writer", "writer",
+    Filename = output_file,
+    Streams = [icetray.I3Frame.Physics, icetray.I3Frame.DAQ])
  
-tray.AddModule("TrashCan", "the can");
+tray.AddModule("TrashCan", "the can")
 
 tray.Execute()
 tray.Finish()
