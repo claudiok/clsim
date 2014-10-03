ecacb3e1178b2769428e80a9279c6c83709d6701
Don LaDieu <nega@icecube.umd.edu>
2014-10-03 19:01:49 +0000

    update for a newer, tighter safeprimes file
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/clsim/trunk@124124 16731396-06f5-0310-8873-f7f720988828

--- resources/download_safeprimes.sh
+++ resources/download_safeprimes.sh
@@ -4,11 +4,11 @@ set -e
 ## DO NOT COPY THIS SCRIPT INTO YOUR PROJECT
 ## Ask on the dataclass e-mail list how to do this
 
-# helper script to automatically download and extract safeprimes_base32.txt
+# helper script to automatically download and extract safeprimes_base32
 # (it takes a long time to build this file..)
 
 BASE_URL="http://code.icecube.wisc.edu/tools/clsim"
-MD5_SUM_EXPECTED="295ae93631b7ff627ae42c1a4b2a7d75"
+MD5_SUM_EXPECTED="911bfc8f19be55a76c5d0ac30c773d0a"
 
 # require exactly one argument
 if [ $# -ne 1 ]; then
@@ -18,7 +18,7 @@ fi
 RESOURCES_DIR=$1
 
 # if the file exists, exit immediately
-if [ -f $RESOURCES_DIR/safeprimes_base32.txt ]; then
+if [ -f $RESOURCES_DIR/safeprimes_base32 ]; then
     # if possible check the md5 checksum to see if the file is ok
     # (if not, just assume it is ok and exit)
     
@@ -30,20 +30,20 @@ if [ -f $RESOURCES_DIR/safeprimes_base32.txt ]; then
     fi
     command -v $md5 >/dev/null && FOUND_MD5=1 || FOUND_MD5=0
     if [ $FOUND_MD5 -eq 1 ]; then
-        MD5_SUM_FILE=`$md5 -q $RESOURCES_DIR/safeprimes_base32.txt`
+        MD5_SUM_FILE=`$md5 -q $RESOURCES_DIR/safeprimes_base32`
     else
         # exit if there is no md5 or md5sum tool available
         command -v md5sum >/dev/null || { exit 0; }
-        md5=`md5sum $RESOURCES_DIR/safeprimes_base32.txt`
+        md5=`md5sum $RESOURCES_DIR/safeprimes_base32`
         MD5_SUM_FILE="${md5%% *}" # remove the first space and everything after it
     fi
     
     if [ "$MD5_SUM_FILE" != "$MD5_SUM_EXPECTED" ]; then
-        echo "file $RESOURCES_DIR/safeprimes_base32.txt does exist, but checksum is not okay! removing and downloading again."
+        echo "file $RESOURCES_DIR/safeprimes_base32 does exist, but checksum is not okay! removing and downloading again."
         echo "     found: $MD5_SUM_FILE"
         echo "  expected: $MD5_SUM_EXPECTED"
         
-        rm $RESOURCES_DIR/safeprimes_base32.txt
+        rm $RESOURCES_DIR/safeprimes_base32
     else
         # checksum is OK!
         exit 0
@@ -51,7 +51,7 @@ if [ -f $RESOURCES_DIR/safeprimes_base32.txt ]; then
 fi 
 
 
-echo "trying to download $RESOURCES_DIR/safeprimes_base32.txt..."
+echo "trying to download $RESOURCES_DIR/safeprimes_base32..."
 
 set +e
 command -v curl >/dev/null && USE_CURL=1 || USE_CURL=0
@@ -66,13 +66,13 @@ set +e
 command -v unxz >/dev/null && USE_XZ=1 || USE_XZ=0
 set -e
 if [ $USE_XZ -eq 1 ]; then
-    FILENAME="safeprimes_base32.txt.xz"
+    FILENAME="safeprimes_base32.xz"
 else
     # xz not found, use .gz file
     # (just assume that gunzip is available,
     # if it is not, this system won't be
     # usable for IceTray anyway.)
-    FILENAME="safeprimes_base32.txt.gz"
+    FILENAME="safeprimes_base32.gz"
 fi
 
 # download
@@ -91,4 +91,4 @@ else
     gunzip $RESOURCES_DIR/$FILENAME
 fi
 
-echo "  $RESOURCES_DIR/safeprimes_base32.txt downloaded."
+echo "  $RESOURCES_DIR/safeprimes_base32 downloaded."
