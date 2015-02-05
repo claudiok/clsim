0b182895ece62dc016f6ed619df341ec0f5eab78
Jakob van Santen <jvansanten@gmail.com>
2015-02-05 19:03:17 +0000

    photomc_2.py is dead, long live photomc.py
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/clsim/branches/better-tabulation@128740 16731396-06f5-0310-8873-f7f720988828

--- resources/tablemaker/photomc.py
+++ resources/tablemaker/photomc.py
@@ -16,6 +16,8 @@ parser.add_option("--zenith", dest="zenith", type="float", default=0.,
     help="Zenith angle of source, in IceCube convention and degrees [%default]")
 parser.add_option("--energy", dest="energy", type="float", default=1,
     help="Energy of light source, in GeV [%default]")
+parser.add_option("--light-source", choices=('cascade', 'flasher', 'infinite-muon'), default='cascade',
+    help="Type of light source. If 'infinite-muon', Z will be ignored, and tracks sampled over all depths. [%default]")
 parser.add_option("--step", dest="steplength", type="float", default=1,
     help="Sampling step length in meters [%default]")
 parser.add_option("--overwrite", dest="overwrite", action="store_true", default=False,
@@ -33,7 +35,7 @@ if path.exists(outfile):
 		parser.error("Output file exists! Pass --overwrite to overwrite it.")
 
 from icecube import icetray
-from icecube.clsim.tablemaker.tabulator import PhotonGenerator, I3TabulatorModule, generate_seed
+from icecube.clsim.tablemaker.tabulator import CombinedPhotonGenerator, generate_seed
 
 outfile = args[0]
 if opts.seed is None:
@@ -44,13 +46,13 @@ from I3Tray import I3Tray
 
 tray = I3Tray()
 
-rng, header = tray.AddSegment(PhotonGenerator, 'generator', Seed=opts.seed,
-    Zenith=opts.zenith, ZCoordinate=opts.z, Energy=opts.energy, NEvents=opts.nevents)
-    
-tray.AddModule(I3TabulatorModule, 'beancounter',
-    Source='Source', Photons='PropagatedPhotons', Statistics='I3CLSimStatistics',
-    Filename=outfile, StepLength=opts.steplength, RandomService=rng,
-    TableHeader=header)
+icetray.logging.set_level_for_unit('I3CLSimStepToTableConverter', 'TRACE')
+icetray.logging.set_level_for_unit('I3CLSimTabulatorModule', 'DEBUG')
+icetray.logging.set_level_for_unit('I3CLSimLightSourceToStepConverterGeant4', 'TRACE')
+icetray.logging.set_level_for_unit('I3CLSimLightSourceToStepConverterFlasher', 'TRACE')
+
+tray.AddSegment(CombinedPhotonGenerator, 'generator', Seed=opts.seed, PhotonSource=opts.light_source,
+    Zenith=opts.zenith, ZCoordinate=opts.z, Energy=opts.energy, NEvents=opts.nevents, Filename=outfile)
     
 tray.AddModule('TrashCan', 'MemoryHole')
 tray.Execute()
