d8f20c0ad99dba16e2519439459c4506a50ff400
Alex Olivas <olivas@icecube.umd.edu>
2016-07-26 16:54:57 +0000

    this doesn't depend directly on I3TreeUtils.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/clsim/trunk@148769 16731396-06f5-0310-8873-f7f720988828

--- private/clsim/util/I3MuonSliceRemoverAndPulseRelabeler.cxx
+++ private/clsim/util/I3MuonSliceRemoverAndPulseRelabeler.cxx
@@ -40,7 +40,6 @@
 #include "dataclasses/physics/I3ParticleID.h"
 #include "dataclasses/physics/I3MCTree.h"
 #include "dataclasses/physics/I3MCTreeUtils.h"
-#include "dataclasses/I3TreeUtils.h"
 #include "dataclasses/I3Constants.h"
 
 #include "simclasses/I3MCPE.h"
