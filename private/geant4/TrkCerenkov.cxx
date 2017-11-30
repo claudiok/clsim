9f1f5f506b523a61fb07e9445d235c49409ee861
nega <nega@16731396-06f5-0310-8873-f7f720988828>
2017-11-30 15:43:24 +0000

    undo bad commit
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/clsim/trunk@159805 16731396-06f5-0310-8873-f7f720988828

--- private/geant4/TrkCerenkov.cxx
+++ private/geant4/TrkCerenkov.cxx
@@ -34,7 +34,6 @@
 //
 ////////////////////////////////////////////////////////////////////////
 
-#include "G4Allocator.hh"
 #include "G4ios.hh"
 #include "G4Poisson.hh"
 #include "G4EmProcessSubType.hh"
